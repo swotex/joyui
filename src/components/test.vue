e5d5af016bcb1c4afce80d34f8cc2ff8f2ef1d84
swotex <swotex21@gmail.com>
2024-12-19 10:35:49 +0100

    turn to unselectable label

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,6 +1,6 @@
 <template>
 	<span id="test" class="relative">
-		<label  class="absolute ml-1 top-[50%] px-1 bg-base-100 translate-y-[-50%]">
+		<label id="labelId" class="absolute ml-1 top-[50%] px-1 bg-base-100 translate-y-[-50%]">
   Name</label>
   <input type="text" class="input input-bordered" placeholder="" />
 	</span>
@@ -15,5 +15,9 @@
 	top: 0;
     transform: translateY(-115%);
 	/* background: ; */
+}
+
+#labelId{
+	pointer-events: none;
 }
 </style>
